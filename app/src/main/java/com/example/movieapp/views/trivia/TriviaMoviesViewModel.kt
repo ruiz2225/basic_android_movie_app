b64365f364d8395020a4c6e5c2a5e9bdf729c0cd
ruiz2225 <jcrc925@gmail.com>
2023-03-17 10:25:53 -0500

    Se encapsulta el LiveData para restringir el acceso directo.

--- app/src/main/java/com/example/movieapp/views/trivia/TriviaMoviesViewModel.kt
+++ app/src/main/java/com/example/movieapp/views/trivia/TriviaMoviesViewModel.kt
@@ -1,24 +1,27 @@
 package com.example.movieapp.views.trivia
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TriviaMoviesViewModel: ViewModel() {
 
-    val score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     init {
-        score.value = 0
+        _score.value = 0
         Log.d("TriviaMoviesViewModel","ViewModel creado")
     }
 
     fun onCorrect(){
-        score.value = score.value?.plus(5)
+        _score.value = score.value?.plus(5)
     }
 
     fun onFailed(){
-        score.value = score.value?.minus(3)
+        _score.value = score.value?.minus(3)
     }
 
     override fun onCleared() {
